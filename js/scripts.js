81c28964041f7f838473595768e95b477137f428
Jordan King and Jake Nagel <jordan@>
2019-07-03 20:45:21 -0700

    set up initial files

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+var movies = ["Spider-Man: Far From Home", "Toy Story 4", "Annabelle Comes Home", "Yesterday", "Aladdin", "Men in Black: International", "The Secret Life of Pets 2", "Stuber", "Crawl", "Avengers: Endgame", "Childs Play", "John Wick: Chapter 3 - Parabellum"]
+
+// business logic
+function Ticket(movieName, movieTime, age, movieRelease) {
+    this.movieName = movieName;
+    this.movieTime = movieTime;
+    this.age = age;
+    this.movieRelease = movieRelease;
+}
+string.prototype.newMovie = function() {return this + 5};
+string.prototype.priceMatinee = function() {return this - 2};
+string.prototype.reducedPriceAge = function() {return this - 3};
+
+// front end logic
